c3188b92a2f2c95e9131339bebd31271940f1e17
Xavimat <1102677+xavi-mat@users.noreply.github.com>
2023-12-13 19:09:52 +0100

    installing subscriptions

---
+++ graphql/pubsub.js
@@ -0,0 +1,6 @@
+// Instrucciones en:
+// https://www.apollographql.com/docs/apollo-server/data/subscriptions/#the-pubsub-class
+
+import { PubSub } from 'graphql-subscriptions';
+
+const pubsub = new PubSub();
